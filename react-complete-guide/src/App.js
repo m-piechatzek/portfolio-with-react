1ecac9fdc12cbdf4050247176eaf000cab2e64a9
MonikaP <piechatzek@hotmail.com>
2020-10-05 13:17:53 -0600

    Added Person comp

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -1,12 +1,15 @@
 import React, { Component } from "react";
 import "./App.css";
+import Person from './Person/Person.js';
+
 class App extends Component {
 	render() {
 		return (
 			<div className="App">
 				<h1>Monika</h1>
-
+				<Person />
 			</div>
+		// return React.createElement('h1', null, 'h1', 'Hi, I\'m aReact App!!!');
 		);
 	}
 }
