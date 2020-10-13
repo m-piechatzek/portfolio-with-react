ae3f2bba90e3261ee93b39235e2e1485d529e143
MonikaP <piechatzek@hotmail.com>
2020-10-12 23:32:40 -0600

    toggle on switch button

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -11,13 +11,13 @@ const app = props => {
             { name: 'kris', age: 5 },
             { name: 'fawnze', age: 19 }
         ]
-
     });
+    const [showPersons, setShowPersons] = useState(false);
+    const [setOtherState, otherState] = useState('somethang else');
 
 
-    const [otherState, setOtherState] = useState('somethang else');
 
-
+    console.log(typeof(setOtherState))
     const switchNameHandler = (newName) => {
 
         setPersonsState({
@@ -41,32 +41,38 @@ const app = props => {
         });
     }
 
+    const togglePersonsHandler = () => setShowPersons(showPersons => !showPersons);
+
+
+    const style = {
+        backgroundColor: 'white',
+        font: 'inherit',
+        border: '1px solid blue',
+        padding: '8px',
+        cursor: 'pointer'
+    };
 
     
-        const style = {
-            backgroundColor: 'white',
-            font: 'inherit',
-            border: '1px solid blue',
-            padding: '8px',
-            cursor: 'pointer'
-        };
-
-    
-
     return (
         <div className="App">
 				<h1>Monika</h1>
-				<Person name={personsState.persons[0].name} age={personsState.persons[0].age} />
-				<Person 
-				name={personsState.persons[1].name} 
-				age={personsState.persons[1].age} 
-				click={switchNameHandler.bind(this,'Fawns in person class')}
-				changed={nameChangedHandler}
-				>hobby</Person>
-				<Person name={personsState.persons[2].name} age={personsState.persons[2].age} />
 				<button 
 				style={style} 
-				onClick={() => switchNameHandler('Monzozo')}>Switch em</button>
+				onClick={togglePersonsHandler}>Switch em</button>
+        {(console.log("showPersons JSX", showPersons))}
+				{ 
+					showPersons ? <div>
+				<Person 
+				 name={personsState.persons[0].name} 
+				 age={personsState.persons[0].age} />
+				<Person 
+				 name={personsState.persons[1].name} 
+				 age={personsState.persons[1].age} 
+				 click={switchNameHandler.bind(this,'Fawns in person class')}
+				 changed={nameChangedHandler}>hobby</Person>
+				<Person name={personsState.persons[2].name} age={personsState.persons[2].age} />
+				</div> : <div></div> 
+			}
 			</div>
         // return React.createElement('h1', null, 'h1', 'Hi, I\'m aReact App!!!');
     );
